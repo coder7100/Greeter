fc07e3c94a8fbce8dfdbe18a53d82e8606386f50
coder7100 <a.aundhkar7100@gmail.com>
2020-03-19 01:12:04 +0530

    adding functions

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 window = tk.Tk()
 
@@ -6,7 +7,20 @@ window.title("greeter")
 # geometry is for setting the size of the window
 window.geometry("400x400")
 
+# --functions----
+
+
+def phrase_genrator():
+    name = str(entry1.get())
+    phrases = ["Hey", "Hello", "Ssup", "Have a Nice Day"]
+    greeting = random.choice(phrases)+name
+    # ---text field
+    greeting_display = tk.Text(master=window, height=10, width=30)
+    greeting_display.grid(column=1, row=3)
+    greeting_display.insert(tk.END, greeting)
+
 # --labels----
+
 
 label1 = tk.Label(text="welcome to greeter ")
 label1.grid(column=1, row=0)
@@ -21,7 +35,7 @@ entry1.grid(column=1, row=1)
 
 # --button
 
-button1 = tk.Button(text="CLICK ME!")
+button1 = tk.Button(text="CLICK ME!", command=phrase_genrator)
 button1.grid(column=1, row=2)
 
 window.mainloop()
