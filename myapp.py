51d10141da767ba22cd211dbe3c44416e77da263
coder7100 <a.aundhkar7100@gmail.com>
2020-03-19 00:37:42 +0530

    adding labels

--- myapp.py
+++ myapp.py
@@ -2,8 +2,16 @@ import tkinter as tk
 
 window = tk.Tk()
 
-window.title("greetings")
+window.title("greeter")
 # geometry is for setting the size of the window
 window.geometry("400x400")
+
+# --labels----
+
+label1 = tk.Label(text="welcome to greeter ")
+label1.grid(column=1, row=0)
+
+label1 = tk.Label(text="Enter your name: ")
+label1.grid(column=0, row=1)
 
 window.mainloop()
